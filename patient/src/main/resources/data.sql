60c0211acb12dc66cb4a8e70074fb180395c488a
Sabouni Youssef <youssefsabouni@icloud.com>
2022-09-12 00:42:11 +0200

    Fix case data

--- patient/src/main/resources/data.sql
+++ patient/src/main/resources/data.sql
@@ -1,13 +1,13 @@
 INSERT INTO patient (id, lastname, firstname, birthdate, gender, address, phone)
-VALUES (1, "dupont", "nicolas", "1989-01-01", "m", "38 rue de la rivière", "+33634560001"),
-       (2, "dupont", "aline", "1988-01-02", "f", "38 rue de la rivière", "+33634560002"),
-       (3, "dupuis", "rené", "1950-01-03", "m", "50 rue dessot", "+33634560003"),
-       (4, "messaoud", "salima", "1985-01-04", "f", "103 bis grand rue", "+33634560004"),
-       (5, "tavares", "gérard", "1999-01-05", "m", "58, rue de l'adresse postal", "+33634560005"),
-       (6, "dupont", "charlotte", "2000-01-06", "f", null, null),
-       (7, "sabini", "sahra", "2002-01-07", "f", "103 bis grand rue", "+33634560007"),
-       (8, "messaoud", "fatima", "2010-01-08", "f", "103 bis grand rue", "+33634560008"),
-       (9, "messaoud", "hassan", "2012-01-09", "m", null, null)
+VALUES (1, "Dupont", "Nicolas", "1989-01-01", "M", "38 rue de la rivière", "+33634560001"),
+       (2, "Dupont", "Aline", "1988-01-02", "F", "38 rue de la rivière", "+33634560002"),
+       (3, "Dupuis", "René", "1950-01-03", "M", "50 rue dessot", "+33634560003"),
+       (4, "Messaoud", "Salima", "1985-01-04", "F", "103 bis grand rue", "+33634560004"),
+       (5, "Tavares", "Gérard", "1999-01-05", "M", "58, rue de l'adresse postal", "+33634560005"),
+       (6, "Dupont", "Charlotte", "2000-01-06", "F", null, null),
+       (7, "Sabini", "Sahra", "2002-01-07", "F", "103 bis grand rue", "+33634560007"),
+       (8, "Messaoud", "Fatima", "2010-01-08", "F", "103 bis grand rue", "+33634560008"),
+       (9, "Messaoud", "Hassan", "2012-01-09", "M", null, null)
 
 ON DUPLICATE KEY
     UPDATE lastname  =
