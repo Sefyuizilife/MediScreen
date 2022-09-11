6145c3b7fe45d38ac64d175b9cb79b94d0f24ef7
Sabouni Youssef <youssefsabouni@icloud.com>
2022-09-11 05:12:00 +0200

    Add test property

--- client/src/test/java/com/mediscreen/client/ClientApplicationTests.java
+++ client/src/test/java/com/mediscreen/client/ClientApplicationTests.java
@@ -2,12 +2,15 @@ package com.mediscreen.client;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
 
 @SpringBootTest
+@TestPropertySource(properties = {"patient.host = test", "note.host = test", "report.host = test"})
 class ClientApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+
+    }
 
 }
