9444eb60b90834f546079c57ef2813fd95d168ae
Sabouni Youssef <youssefsabouni@icloud.com>
2022-09-14 04:37:02 +0200

    Change of the returns of the proxies to have the body & change url

--- report/src/main/java/com/mediscreen/report/proxies/NoteProxy.java
+++ report/src/main/java/com/mediscreen/report/proxies/NoteProxy.java
@@ -3,7 +3,6 @@ package com.mediscreen.report.proxies;
 import com.mediscreen.report.models.Note;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -17,11 +16,11 @@ import java.util.List;
 public interface NoteProxy {
 
     @GetMapping("/all/patients/{patientId}")
-    ResponseEntity<List<Note>> browseByPatientId(@PathVariable Long patientId);
+    List<Note> browseByPatientId(@PathVariable Long patientId);
 
-    @PostMapping(value = "/add", consumes = MediaType.APPLICATION_FORM_URLENCODED_VALUE)
-    ResponseEntity<Note> create(@Valid Note note);
+    @PostMapping(value = "", consumes = MediaType.APPLICATION_FORM_URLENCODED_VALUE)
+    Note create(@Valid Note note);
 
     @PutMapping(value = "/{id}", consumes = MediaType.APPLICATION_FORM_URLENCODED_VALUE)
-    ResponseEntity<Note> update(@PathVariable BigInteger id, Note note);
+    Note update(@PathVariable BigInteger id, Note note);
 }
